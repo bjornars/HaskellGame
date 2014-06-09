3d5e0456d1f37c1ed411febaaadb271f3acf010e
Martin Øinæs Myrseth <myrseth@gmail.com>
2014-06-09 10:24:49 +0200

    Z is for Zombie

--- src/Actor/Zombie.hs
+++ src/Actor/Zombie.hs
@@ -6,7 +6,7 @@ import Types
 
 
 zombieImg :: Image
-zombieImg = string (with_fore_color def_attr bright_red) "X"
+zombieImg = string (with_fore_color def_attr bright_red) "Z"
 
 
 zombie :: Coords -> Actor ()
