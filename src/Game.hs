26d94fc15aa179c1db4fa87507383b28723ae594
Martin Øinæs Myrseth <myrseth@gmail.com>
2014-06-10 08:27:01 +0200

    Only draw before we begin processing the next tick
    Collect processed actors in xs' which we reverse and evaluate for the
    next tick cycle. When xs' is empty we know we're at the start of a new
    tick cycle and it makes sense to draw the result of the last evaluated
    tick.

--- src/Game.hs
+++ src/Game.hs
@@ -2,21 +2,24 @@
 
 module Game (startGame) where
 
+import Control.Monad
 import Control.Monad.Operational
 import Data.Array.IArray
 import Types
 import qualified Levels.Level1 as L1
 
 startGame :: (Level -> IO ()) -> IO GAction -> IO ()
-startGame draw getInput = uncurry go L1.level
+startGame draw getInput = uncurry go L1.level []
     where
     -- Main loop
-    go _ []           = return ()
-    go level (actor : xs) = do
+    go _     []           []  = return ()
+    go level []           xs  = go level (reverse xs) []
+    go level (actor : xs) xs' = do
+        when (null xs') $ draw level
         cont <- evalActor level actor
         case cont of
             Nothing               -> return ()
-            Just (level', actor') -> go level' (xs ++ [actor'])
+            Just (level', actor') -> go level' xs (actor' : xs')
 
     -- Handle actor actions
     evalActor level actor =
@@ -30,7 +33,6 @@ startGame draw getInput = uncurry go L1.level
                 return $ Just (level, actor { actorProg = next level })
 
             (GetUserAction :>>= next) -> do
-                draw level
                 action <- getInput
                 return $ Just (level, actor { actorProg = next action })
 
