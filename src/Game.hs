6f22e1fa1095090a625656da307f8c8878127269
Bjørnar Snoksrud <bjornar@snoksrud.no>
2014-06-11 10:52:02 +0200

    Use Data.Sequence for actor list.
    It gets worse before it gets better.

--- src/Game.hs
+++ src/Game.hs
@@ -6,60 +6,67 @@ import Control.Arrow ((&&&))
 import Control.Monad.Operational
 import Data.Array.IArray hiding (range)
 import System.Random (randomRIO)
+import Data.Sequence ((|>), (<|), fromList, Seq)
+import Data.Foldable (toList)
+import qualified Data.Sequence as S
+
 import Types
 import qualified Levels.Level1 as L1
 
 startGame :: (Level -> IO ()) -> IO GAction -> IO ()
 startGame draw getInput =
     let (level, actors) = L1.level in
-    go level (drawActor:actors)
+    go level $ fromList (drawActor:actors)
     where
     -- Main loop
-    go _     [] = return ()
-    go level (actor : xs) = do
-        let actors = actor : xs
-        cont <- eval actors level actor
+    go level actors
+        | S.null actors  = return ()
+        | otherwise = do
+        cont <- eval level actors
         case cont of
-            Nothing               -> return ()
-            Just (level', actor') -> go level' $ xs ++ [actor']
+            Nothing      -> return ()
+            Just actors' -> go level actors'
 
     -- Handle actor actions
-    eval actors level = evalActor
+    eval level = evalActor
         where
-        evalActor actor = case view $ actorProg actor of
+        evalActor actors = let
+                actor = S.index actors 0
+                actors' = S.drop 1 actors in case view $ actorProg actor of
             (NextTick :>>= next) ->
             -- this actor is done, move on to next actor
-                return $ Just (level, actor { actorProg = next () })
+                return $ Just $ actors' |>  actor { actorProg = next () }
 
             (Return _) -> return Nothing
 
             (GetRandom range :>>= next) -> do
                 randVal <- randomRIO range
-                evalActor actor { actorProg = next randVal }
+                evalActor $ actor { actorProg = next randVal } <| actors'
 
             (ReadMap :>>= next) ->
-                evalActor actor { actorProg = next level }
+                evalActor $ actor { actorProg = next level } <| actors'
 
             (ReadMapWithActors :>>= next) ->
-                evalActor actor { actorProg = next $ addActors actors level }
+                evalActor $ actor { actorProg = next $ addActors actors level } <| actors'
 
             (GetUserAction :>>= next) -> do
                 action <- getInput
-                evalActor actor { actorProg = next action }
+                evalActor $ actor { actorProg = next action } <| actors'
 
             (GetActorPosition :>>= next) ->
-                evalActor actor { actorProg = next $ actorPos actor }
+                evalActor $ actor { actorProg = next $ actorPos actor } <| actors'
 
             (MoveActor new :>>= next) ->
                 let nextActor = actor { actorProg = next () }
                 in case addActors actors level ! new of
-                    Empty -> evalActor nextActor { actorPos = new }
-                    _     -> evalActor nextActor
+                    Empty -> evalActor $ nextActor { actorPos = new } <| actors'
+                    _     -> evalActor $ nextActor <| actors'
 
             (DrawMap :>>= next) -> do
                 draw $ addActors actors level
-                evalActor actor { actorProg = next () }
+                evalActor $ actor { actorProg = next () } <| actors'
 
 
-addActors :: [Actor ()] -> Level -> Level
-addActors actors level = level // map (actorPos &&& ActorBlock . actorImage) actors
+addActors :: Seq (Actor ()) -> Level -> Level
+addActors actors level = let actors' = toList actors in
+    level // map (actorPos &&& ActorBlock . actorImage) actors'
