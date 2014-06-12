6114bfd523900212de13f2fb0862a56dd1bcd274
Martin Øinæs Myrseth <myrseth@gmail.com>
2014-06-13 01:24:18 +0200

    Switch `Void` and `Empty` console styles
    More pleasing to look at

--- src/Console.hs
+++ src/Console.hs
@@ -22,8 +22,8 @@ draw vty level = do
 
 
 drawCell :: Block -> Image
-drawCell Empty              = string (with_fore_color def_attr bright_black) "."
-drawCell Void               = string def_attr " "
+drawCell Void               = string (with_fore_color def_attr bright_black) "."
+drawCell Empty              = string def_attr " "
 drawCell Wall               = string (with_fore_color def_attr bright_black) "X"
 drawCell (ActorBlock actor) = actorImage actor
 
