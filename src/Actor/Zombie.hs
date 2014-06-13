18edadaf071dd1c567c020cf247fc52afbe7a50a
Martin Øinæs Myrseth <myrseth@gmail.com>
2014-06-14 00:22:23 +0200

    Make Zombies track the last known hero position
    Makes their behavior a lot more natural as they won't try to walk the
    shortest path through a wall to reach the hero.

--- src/Actor/Zombie.hs
+++ src/Actor/Zombie.hs
@@ -17,30 +17,41 @@ zombieImg = string (with_fore_color def_attr bright_green) "Z"
 awareZombieImg :: Image
 awareZombieImg = string (with_fore_color def_attr bright_red) "Z"
 
-data ZombieState = ZombieState {
-    timeSinceHasSeen :: Integer
-}
+data ZombieState = ZombieState
+                 { forgetTimer :: Integer
+                 , trackPoint  :: Maybe Coords
+                 }
+
+initState :: ZombieState
+initState = ZombieState 0 Nothing
 
 zombie :: Coords -> Actor ()
-zombie initPos = (ActorData zombieImg initPos False, prog (ZombieState 999))
+zombie initPos = (ActorData zombieImg initPos False, prog initState)
     where
     rememberActorTurns = 7
     prog state = do
         pos   <- getActorPosition
         level <- readMapWithActors
         hero  <- actorPos . head . filter actorIsPlayer <$> getOtherActors
-        let canSee = canSeeCoord level pos hero
-            timeSinceHasSeen' = if canSee then 0 else timeSinceHasSeen state + 1
 
-        dyx   <- if canSee || timeSinceHasSeen state < rememberActorTurns
-                  then do
-                      setActorImage awareZombieImg
-                      return $ findMove level pos hero
-                  else do
-                      setActorImage zombieImg
-                      (moveDirs !!) <$> getRandom (0, length moveDirs - 1)
+        let canSee = canSeeCoord level pos hero
+            timer  = if canSee then rememberActorTurns else forgetTimer state - 1
+            target | canSee    = Just hero
+                   | timer > 0 = trackPoint state
+                   | otherwise = Nothing
+
+        dyx <- case target of
+            Just target' -> do
+                setActorImage awareZombieImg
+                return $ findMove level pos target'
+            Nothing -> do
+                setActorImage zombieImg
+                (moveDirs !!) <$> getRandom (0, length moveDirs - 1)
+
         moveActor $ pos |+| dyx
-        nextTick >> prog state { timeSinceHasSeen = timeSinceHasSeen' }
+        nextTick >> prog state { forgetTimer = timer
+                               , trackPoint  = target
+                               }
 
 
 moveDirs :: [Coords]
