31e82f47d7324f9fec8b48d7f84ba60935707227
Bjørnar Snoksrud <bjornar@snoksrud.no>
2014-06-13 13:24:26 +0200

    kill a bit less indiscriminately

--- src/Actor/Player.hs
+++ src/Actor/Player.hs
@@ -1,9 +1,12 @@
 module Actor.Player (player) where
 
 import Control.Applicative
+import Control.Monad
 import Graphics.Vty
 import Types
 
+import Data.List
+import Data.Ord
 
 playerImg :: Image
 playerImg = string (with_fore_color def_attr bright_blue) "@"
@@ -12,6 +15,7 @@ playerImg = string (with_fore_color def_attr bright_blue) "@"
 player :: Coords -> Actor ()
 player initPos = (ActorData playerImg initPos True, prog)
     where
+    distance target = vecLen . (|-| target)
     prog = do
         action <- getUserAction
         case action of
@@ -21,8 +25,12 @@ player initPos = (ActorData playerImg initPos True, prog)
                 moveActor $ pos |+| dyx dir
                 nextTick >> prog
             Attack -> do
-                zombie  <- head . filter (not . actorIsPlayer) <$> getOtherActors
-                hurtActor (zombie, 10)
+                pos <- getActorPosition
+                zombies  <- filter (not . actorIsPlayer) <$> getOtherActors
+                -- zombie or treasure, is more like it.. or DrawActor, which is a tad unfortunate
+                unless (null zombies) $
+                    let zombie = minimumBy (comparing $ distance pos . actorPos) zombies in
+                    hurtActor (zombie, 10)
                 nextTick >> prog
             _ -> prog
     dyx DUp    = (-1,  0)
